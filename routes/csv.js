45d23fdc39499c8b818510951fa165084ca42f41
Tanukium <tanukium@gmail.com>
2018-10-06 01:11:05 +0900

    improve function saving table with csv

--- routes/csv.js
+++ routes/csv.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let router = express.Router();
 
-router.post('/', function(req, res, next) {
-  console.log(req.body)
+router.post('/', function (req, res) {
+  let tableToCsv = () => {
+    let str = "";
+    for (i = 0; i < req.body.row; i++) {
+      let arr = [];
+      for (j = 0; j < req.body.col; j++) {
+        arr.push(req.body[i + ":" + j]);
+      }
+      str += arr.join(",") + "\n";
+    }
+    return (str);
+  }
+  res.set({
+    'Content-disposition': 'attachment; filename=save.csv',
+    'Content-Type': 'text/csv'
+  });
+  res.send(tableToCsv());
 });
 
 module.exports = router;
